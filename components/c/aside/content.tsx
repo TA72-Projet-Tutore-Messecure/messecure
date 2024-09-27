6c3f19a82f2ba5fa668540774dc99f6ce6094de6
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2024-09-27 09:57:11 +0200

    Added IUser interface and light theme

--- components/c/aside/content.tsx
+++ components/c/aside/content.tsx
@@ -2,10 +2,14 @@
 
 import { useState } from "react";
 import { CAsideConversation } from "@/components/c/aside/conversation";
+import { createRandomUsers } from "@/services/UserService";
 
 export const CAsideContent: React.FC = () => {
 			// The activeIndex state will either be a number or null (for no active conversation)
 			const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+			// Generate users only once using useState
+			const [users] = useState(() => createRandomUsers(200));
 
 			// TypeScript will infer that index is a number here
 			const handleClick = (index: number) => {
@@ -14,8 +18,9 @@ export const CAsideContent: React.FC = () => {
 
 			return (
 									<div className="w-full max-w-full flex flex-col items-center py-2 px-2 overflow-y-auto">
-												{[...Array(15)].map((_, index: number) => (
+												{users.map((user, index: number) => (
 																		<CAsideConversation
+																								user={user}
 																								key={index}
 																								active={activeIndex === index} // Check if this conversation is active
 																								onClick={() => handleClick(index)} // Set this conversation as active on click
--- components/c/aside/conversation.tsx
+++ components/c/aside/conversation.tsx
@@ -3,24 +3,26 @@
 import { Avatar } from "@nextui-org/react";
 import Ripples from 'react-ripples';
 import { FC } from "react";
+import {IUser} from "@/entities/IUser";
 
 // Define the prop types for CAsideConversation
 interface CAsideConversationProps {
+			user: IUser;
 			active: boolean;
 			onClick: () => void;
 }
 
-export const CAsideConversation: FC<CAsideConversationProps> = ({ active, onClick }) => {
+export const CAsideConversation: FC<CAsideConversationProps> = ({ user, active, onClick }) => {
 			return (
+									// @ts-ignore
 									<Ripples
 															during={1400}
 															color={"rgba(0, 0, 0, 0.1)"}
 															onClick={onClick}
-															className={`w-full max-w-sm py-2 px-3 flex flex-row gap-2 items-center rounded-xl cursor-pointer flex-shrink-0
-               ${active ? 'bg-[#8472dc]' : 'hover:bg-[#2c2c2c]'}`} placeholder={undefined} onPointerEnterCapture={undefined}
-															onPointerLeaveCapture={undefined}									>
-												<Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" className="w-14 h-14 min-w-14 min-h-14 text-small" />
-												<div className="flex flex-col justify-between items-start w-full max-w-[18vw]">
+															className={`w-full max-w-sm py-2 px-3 flex flex-row gap-3 items-center rounded-xl cursor-pointer flex-shrink-0
+               ${active ? 'bg-[#8472dc]' : 'hover:bg-[#f4f4f5] dark:hover:bg-[#2c2c2c]'}`}>
+												<Avatar src={user.avatarUrl} className="w-14 h-14 min-w-14 min-h-14 text-small" />
+												<div className="flex flex-col justify-between items-start w-full max-w-[17vw]">
 															<div className="w-full flex flex-row items-center justify-between">
 																		<span className="text-sm font-bold truncate">Madoc Lefèvre</span>
 																		<span className="text-xs text-white/60">12:00</span>
