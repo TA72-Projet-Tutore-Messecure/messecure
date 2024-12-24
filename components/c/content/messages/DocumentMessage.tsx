f3fa24839f2c4d841343c031589fa5095e24b3c7
Martin GRUDLER <devel@grudler.eu>
2024-12-24 09:23:54 +0100

    Add context menu and delete action to DocumentMessage
    Integrated a dropdown context menu with right-click functionality for messages. Added support for deleting messages, including UI updates and a toast notification. Ensured proper handling of actions based on the message sender and redacted status.

--- components/c/content/messages/DocumentMessage.tsx
+++ components/c/content/messages/DocumentMessage.tsx
@@ -2,29 +2,75 @@
 
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { CheckIcon } from "@nextui-org/shared-icons";
 import { CheckCheckIcon, Clock } from "lucide-react";
-import { FaFile } from "react-icons/fa";
+import { FaFile, FaTrash } from "react-icons/fa";
 import { toast } from "react-hot-toast";
 
 import {
   DocumentMessageProps,
   MessageStatus,
-  MessageTarget,
+  MessageTarget
 } from "@/types/messages";
 import MatrixService from "@/services/MatrixService";
+import { useMatrix } from "@/context/MatrixContext";
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
 
 export const DocumentMessage: React.FC<DocumentMessageProps> = ({
-  eventId,
-  isRedacted,
-  time,
-  target,
-  status,
-  documentName,
-  documentSize,
-  documentLink,
-}) => {
+                                                                  eventId,
+                                                                  isRedacted,
+                                                                  time,
+                                                                  target,
+                                                                  status,
+                                                                  documentName,
+                                                                  documentSize,
+                                                                  documentLink
+                                                                }) => {
+  const { deleteMessage } = useMatrix();
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const handleContextMenu = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsPopoverOpen(true);
+  };
+
+  const handleClosePopover = () => {
+    setIsPopoverOpen(false);
+  };
+
+  const handleDeleteMessage = () => {
+    if (eventId) {
+      deleteMessage(eventId);
+      toast.error("Message deleted.");
+    }
+    handleClosePopover();
+  };
+
+  const actions = [];
+
+  if (target === MessageTarget.SENDER && !isRedacted) {
+    actions.push(
+      <DropdownItem
+        key="deleteMessage"
+        color="danger"
+        onClick={handleDeleteMessage}
+      >
+        <span className="flex flex-row gap-2 items-center">
+          <FaTrash /> Delete Message
+        </span>
+      </DropdownItem>
+    );
+  }
+
+  if (actions.length === 0) {
+    actions.push(
+      <DropdownItem key="noAction" isReadOnly>
+        No actions available.
+      </DropdownItem>
+    );
+  }
+
   // Function to handle document click and download
   const handleDocumentClick = async () => {
     try {
@@ -105,30 +151,41 @@ export const DocumentMessage: React.FC<DocumentMessageProps> = ({
       }`}
       data-event-id={eventId} // Utilizing eventId as a data attribute
     >
-      <div
-        className={`relative base-message ${
-          target === MessageTarget.SENDER
-            ? "message-sender text-black bg-[#eeffde] dark:text-white dark:!bg-[#766ac8] rounded-l-xl rounded-t-xl"
-            : "bg-white dark:bg-[#212121] rounded-xl"
-        } flex flex-col gap-1 max-w-[33%] py-3 px-3`}
+      <Dropdown
+        classNames={{
+          base: "before:bg-default-200", // change arrow background
+          content: "py-1 px-1 backdrop-blur-md bg-opacity-80" // change dropdown content
+        }}
+        isOpen={isPopoverOpen}
+        placement="right"
+        onOpenChange={setIsPopoverOpen}
       >
-        <button
-          className="message-content document-message flex items-center gap-2 w-full h-full"
-          onClick={handleDocumentClick}
-        >
-          <div className="document-icon text-2xl">
-            <FaFile />
-          </div>
-          <div className="document-info max-w-60">
-            <div className="document-name font-semibold truncate">
-              {documentName}
-            </div>
-            <div className="document-size text-left text-sm">
-              {documentSize}
-            </div>
-          </div>
-        </button>
-        <div className="message-info w-full flex flex-row justify-end items-center gap-1">
+        <DropdownTrigger>
+          <div
+            className={`relative base-message ${
+              target === MessageTarget.SENDER
+                ? "message-sender text-black bg-[#eeffde] dark:text-white dark:!bg-[#766ac8] rounded-l-xl rounded-t-xl"
+                : "bg-white dark:bg-[#212121] rounded-xl"
+            } flex flex-col gap-1 max-w-[33%] py-3 px-3`}
+            onContextMenu={handleContextMenu}
+          >
+            <button
+              className="message-content document-message flex items-center gap-2 w-full h-full"
+              onClick={handleDocumentClick}
+            >
+              <div className="document-icon text-2xl">
+                <FaFile />
+              </div>
+              <div className="document-info max-w-60">
+                <div className="document-name font-semibold truncate">
+                  {documentName}
+                </div>
+                <div className="document-size text-left text-sm">
+                  {documentSize}
+                </div>
+              </div>
+            </button>
+            <div className="message-info w-full flex flex-row justify-end items-center gap-1">
           <span
             className={`message-time ${
               target === MessageTarget.SENDER
@@ -138,21 +195,26 @@ export const DocumentMessage: React.FC<DocumentMessageProps> = ({
           >
             {time}
           </span>
-          {target === MessageTarget.SENDER && (
-            <span className="message-status">
+              {target === MessageTarget.SENDER && (
+                <span className="message-status">
               {status === MessageStatus.SENT && (
                 <Clock className="w-3 text-[#4eb25b] dark:text-white font-bold" />
               )}
-              {status === MessageStatus.DELIVERED && (
-                <CheckIcon className="text-[#4eb25b] dark:text-white font-bold" />
-              )}
-              {status === MessageStatus.READ && (
-                <CheckCheckIcon className="w-4 text-[#4eb25b] dark:text-white font-bold" />
-              )}
+                  {status === MessageStatus.DELIVERED && (
+                    <CheckIcon className="text-[#4eb25b] dark:text-white font-bold" />
+                  )}
+                  {status === MessageStatus.READ && (
+                    <CheckCheckIcon className="w-4 text-[#4eb25b] dark:text-white font-bold" />
+                  )}
             </span>
-          )}
-        </div>
-      </div>
+              )}
+            </div>
+          </div>
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Message Actions" variant="faded">
+          {actions}
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 };
